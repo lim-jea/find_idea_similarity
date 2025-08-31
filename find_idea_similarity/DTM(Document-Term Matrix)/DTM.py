b4090a9a4cc9a9ba95b264e45b3676da52fdbec5
lim-jea <jeayoung2942@naver.com>
2025-08-31 17:08:02 +0900

    Chore: DTM 문서 단어 행렬 추가

---
+++ find_idea_similarity/DTM(Document-Term Matrix)/DTM.py
@@ -0,0 +1,48 @@
+import csv
+import numpy
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize 
+from konlpy.tag import Okt
+
+word_to_index = {}
+bow = []
+
+def build_bag_of_words(morphs):
+    """BoW 생성"""
+
+    for word in morphs:  
+        if word not in word_to_index.keys():
+            word_to_index[word] = len(word_to_index)  
+            # BoW에 전부 기본값 1을 넣는다.
+            bow.insert(len(word_to_index) - 1, 1)
+        else:
+            # 재등장하는 단어의 인덱스
+            index = word_to_index.get(word)
+            # 재등장한 단어는 해당하는 인덱스의 위치에 1을 더한다.
+            bow[index] = bow[index] + 1
+    return word_to_index, bow
+
+csv_path = r'c:\Users\jeayy\Desktop\NLP\find_idea_similarity\DTM(Document-Term Matrix)\coala_ai_response_30013_ML.csv'
+with open(csv_path, newline='', encoding='utf-8') as csvfile:
+    reader = csv.DictReader(csvfile)
+    data = [row for row in reader]
+
+# 파일 경로를 문자열로 전달 (쉼표 제거)
+stopwords_path = r'c:\Users\jeayy\Desktop\NLP\find_idea_similarity\DTM(Document-Term Matrix)\stopwords-ko.txt'
+with open(stopwords_path, encoding='utf-8') as f:
+    stop_words_list = [line.strip() for line in f if line.strip()]
+stop_words_list.extend(['.','아이디어',','])
+stop_words_set = set(stop_words_list)
+
+okt = Okt()
+
+for row in data:
+    idea_text = row['idea']
+    morphs = okt.morphs(idea_text)
+    for morph in morphs:
+        if morph in stop_words_set:
+            morphs.remove(morph)
+    row['idea_ morphs'] = morphs
+    build_bag_of_words(morphs)
+print(word_to_index)
+print(bow)
---
+++ find_idea_similarity/DTM(Document-Term Matrix)/README.txt
@@ -0,0 +1,2 @@
+stopwords-ko.txt 불용어 리스트 참조
+https://gist.github.com/spikeekips/40eea22ef4a89f629abd87eed535ac6a
